98a65f8766bc95384c6ad778b38020ec7832a359
jheiser38 <jheiser38@gmail.com>
2021-07-13 13:51:48 -0400

    Revised api auth testing.

--- api/tests/test_api_auth.py
+++ api/tests/test_api_auth.py
@@ -10,7 +10,7 @@ from app.models import User, Role, Family
 # This will load a user and family for testing.
 # This is not included in teh setup since some tests require testing these
 #       elements individually.
-def load_user(with_family=False, confirmed=True):
+def load_user(with_family=False, confirmed=True, other_user=False):
     f = Family(family_name='f')
     db.session.add(f)
     db.session.commit()
@@ -21,6 +21,12 @@ def load_user(with_family=False, confirmed=True):
              confirmed=confirmed)
     db.session.add(u)
     db.session.commit()
+    if other_user:
+        u2 = User(username='u2', email='u2', password='u2',
+                 family_id=f.id, confirmed=True)
+        db.session.add(u2)
+        db.session.commit()
+        return u,f,u2
     return u,f
 
 
@@ -149,7 +155,7 @@ class APITestCase(unittest.TestCase):
 
 
     # Test the /api/auth/registration route.
-    def test_registration(self):
+    def test_registerUser(self):
         u,f = load_user(True)
 
         # Test username and email unavailable.
@@ -274,7 +280,7 @@ class APITestCase(unittest.TestCase):
         self.assertEqual(response.get_json()['message'], 'User u already confirmed.')
 
 
-    # Tests the /api/auth.sendFamilyInvite route.
+    # Tests the /api/auth/sendFamilyInvite route.
     def test_sendFamilyInvite(self):
         u,f = load_user(True, False)
         token = u.generate_auth_token()
@@ -291,7 +297,7 @@ class APITestCase(unittest.TestCase):
 
 
     # Tests the /api/auth/confirmInviteToken/<token> route.
-    def test_confirmInviteToken(self):
+    def test_confirmJoinFamily(self):
         u,f = load_user(False, False)
         token = f.generate_family_token('u')
         self.assertIsNone(u.family)
@@ -379,6 +385,16 @@ class APITestCase(unittest.TestCase):
         u,f = load_user()
         token = u.generate_confirmation_token()
 
+        # Test for invalid token.
+        post_body = '{"token":' + 'None' + ',"password":"new"}'
+        response = self.client.post('/api/auth/processPasswordReset',
+                data=json.dumps({
+                    'auth':{"email_or_token":""},
+                    'body':post_body
+                }),
+                content_type='application/json')
+        self.assertEqual(response.status_code,401)
+
         # Test for valid token.
         post_body = '{"token":' + f'{token}' + ',"password":"new"}'
         response = self.client.post(f'/api/auth/validateResetRequest/{token}',
@@ -388,3 +404,209 @@ class APITestCase(unittest.TestCase):
                 }),
                 content_type='application/json')
         self.assertEqual(response.status_code, 200)
+
+    # Tests the /api/auth/removeFamilyMember route.
+    def test_removeFamilyMember(self):
+        u,f,u2 = load_user(True,True,True)
+        token = u.generate_auth_token()
+
+        # Test for invalid user id.
+        post_body = '{"id":' + '""}'
+        response = self.client.post('/api/auth/removeFamilyMember',
+                data=json.dumps({
+                    'auth':{"email_or_token":token},
+                    'body':post_body
+                }),
+                content_type='application/json')
+        self.assertEqual(response.status_code, 404)
+
+        # Test for valid user id.
+        self.assertEqual(u2.family_id, f.id)
+        post_body = '{"id":' + f'{u2.id}' + '}'
+        response = self.client.post('/api/auth/removeFamilyMember',
+                data=json.dumps({
+                    'auth':{"email_or_token":token},
+                    'body':post_body
+                }),
+                content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(u2.family_id, f.id)
+        self.assertEqual(response.get_json(),{'user_removed':f'{u2.username}'})
+
+    # Tests the /api/auth/makeLeader route.
+    def test_makeLeader(self):
+        u,f,u2 = load_user(True, True, True)
+        token = u.generate_auth_token()
+
+        # Test for invalid user id.
+        post_body = '{"id":' + '""}'
+        response = self.client.post('/api/auth/makeLeader',
+                data=json.dumps({
+                    'auth':{"email_or_token":token},
+                    'body':post_body
+                }),
+                content_type='application/json')
+        self.assertEqual(response.status_code, 404)
+
+        # Test for valid user id.
+        self.assertEqual(u2.role_id, Role.query.filter_by(name='User').first().id)
+        post_body = '{"id":' + f'{u2.id}' + '}'
+        response = self.client.post('/api/auth/makeLeader',
+                data=json.dumps({
+                    'auth':{"email_or_token":token},
+                    'body':post_body
+                }),
+                content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(u2.role_id, Role.query.filter_by(name='Leader').first().id)
+        self.assertEqual(response.get_json(),{'increased_privileges':f'{u2.username}'})
+
+    # Tests the /api/auth/unmakeLeader route.
+    def test_unmakeLeader(self):
+        u,f,u2 = load_user(True, True, True)
+        token = u.generate_auth_token()
+
+        # Elevate u2 privaledges for test.
+        u2.role = Role.query.filter_by(name='Leader').first()
+        db.session.add(u2)
+        db.session.commit()
+
+        # Test for invalid user id.
+        post_body = '{"id":' + '""}'
+        response = self.client.post('/api/auth/unmakeLeader',
+                data=json.dumps({
+                    'auth':{"email_or_token":token},
+                    'body':post_body
+                }),
+                content_type='application/json')
+        self.assertEqual(response.status_code, 404)
+
+        # Test for valid user id.
+        post_body = '{"id":' + f'{u2.id}' + '}'
+        response = self.client.post('/api/auth/unmakeLeader',
+                data=json.dumps({
+                    'auth':{"email_or_token":token},
+                    'body':post_body
+                }),
+                content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(u2.role_id, Role.query.filter_by(name='User').first().id)
+        self.assertEqual(response.get_json(),{'reduced_privileges':f'{u2.username}'})
+
+    # Test the /api/auth/changeEmailRequest route.
+    def test_sendChangeEmailRequest(self):
+        u,f = load_user()
+        token = u.generate_auth_token()
+
+        # Test invalid email.
+        post_body = '{"email":' + 'None' + '}'
+        response = self.client.post('/api/auth/changeEmailRequest',
+                data=json.dumps({
+                    'auth':{"email_or_token":token},
+                    'body':post_body
+                }),
+                content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+        # Test valid email.
+        post_body = '{"email":' + '"new_email"' + '}'
+        response = self.client.post('/api/auth/changeEmailRequest',
+                data=json.dumps({
+                    'auth':{"email_or_token":token},
+                    'body':post_body
+                }),
+                content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+    # Test the /api/auth/confirmChangeEmail route.
+    def test_confirmChangeEmail(self):
+        u,f,u2 = load_user(True, True, True)
+
+        # Test invalid token .
+        token = 'Invalid'
+        post_body = ''
+        response = self.client.post(f'/api/auth/confirmChangeEmail/{token}',
+        data=json.dumps({
+                'auth':{"email_or_token":""},
+                'body':post_body
+            }),
+            content_type='application/json')
+        self.assertEqual(response.status_code, 401)
+
+        # Test valid token.
+        token = u.generate_email_token("new_email")
+        post_body = ''
+        response = self.client.post(f'/api/auth/confirmChangeEmail/{token}',
+        data=json.dumps({
+                'auth':{"email_or_token":""},
+                'body':post_body
+            }),
+            content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(u.email,"new_email")
+
+        # Test valid token, but unavailable email.
+        token = u2.generate_email_token("new_email")
+        post_body = ''
+        response = self.client.post(f'/api/auth/confirmChangeEmail/{token}',
+        data=json.dumps({
+                'auth':{"email_or_token":""},
+                'body':post_body
+            }),
+            content_type='application/json')
+        self.assertEqual(response.status_code, 207)
+        self.assertEqual(u2.email,"u2")
+
+    # Test the /api/auth/createFamily route.
+    def test_createFamily(self):
+        u,f = load_user()
+        token = u.generate_auth_token()
+
+        # Test for invalid request.
+        post_body = '{"familyName":""}'
+        response = self.client.post('/api/auth/createFamily',
+                data=json.dumps({
+                    'auth':{"email_or_token":token},
+                    'body':post_body
+                }),
+                content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+        # Test for valid request.
+        post_body = '{"familyName":' + '"new_family"}'
+        response = self.client.post('/api/auth/createFamily',
+                data=json.dumps({
+                    'auth':{"email_or_token":token},
+                    'body':post_body
+                }),
+                content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(u.family, Family.query.filter_by(family_name="new_family").first())
+
+    # Test the /api/auth/changePassword route.
+    def test_changePassword(self):
+        u,f = load_user()
+        token = u.generate_auth_token()
+
+        # Test for invalid password.
+        post_body = '{"oldPass":"bum_password","newPass":"new_pass"}'
+        response = self.client.post('/api/auth/changePassword',
+                data=json.dumps({
+                    'auth':{"email_or_token":token},
+                    'body':post_body
+                }),
+                content_type='application/json')
+        self.assertEqual(response.status_code, 401)
+
+        # Test for valid password.
+        old_hash = u.password_hash
+        post_body = '{"oldPass":"u","newPass":"new_pass"}'
+        response = self.client.post('/api/auth/changePassword',
+                data=json.dumps({
+                    'auth':{"email_or_token":token},
+                    'body':post_body
+                }),
+                content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(old_hash, u.password_hash)
+        self.assertTrue(u.verify_password("new_pass"))
